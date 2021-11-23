ab942779cba14d13a59b020d455e470ddb56fa2a
renydiaz02 <reny_diaz@ucne.edu.do>
2021-11-23 17:57:32 -0400

    Creando la entidad cuota

---
+++ RegistroDePrestamo/Entidades/Cuota.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDePrestamo.Entidades
+{
+    public class Cuota
+    {
+        [Key]
+
+        public int IdCuota { get; set; }
+        public Prestamos oPrestamo { get; set; }
+        public int NumeroCuota { get; set; }
+        public string FechaPagoCuota { get; set; }
+        public float MontoCuota { get; set; }
+        public string EstadoCuota { get; set; }
+        public int ProximoPago { get; set; }
+    }
+}
