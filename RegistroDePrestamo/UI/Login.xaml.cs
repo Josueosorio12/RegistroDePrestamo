3ed210a9bfd1ac733efcdb14d9c6a8ae07af326f
Josueosorio12 <josue_osorio@ucne.edu.do>
2021-11-28 11:45:40 -0400

    Creando el registro del Login

---
+++ RegistroDePrestamo/UI/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using RegistroDePrestamo.BLL;
+using RegistroDePrestamo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RegistroDePrestamo.UI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        Prestamos prestamos = new Prestamos();
+        MainWindow Principal = new MainWindow();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void IngresarButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool paso = LoginBLL.Validar(NombreUsuarioTextBox.Text, ContrasenaPasswordBox.Password);
+
+            if (paso)
+            {
+                this.Close();
+                Principal.Show();
+            }
+            else
+            {
+                MessageBox.Show("Nombre Usuario o Contraseña incorrecta!", "Error!");
+                ContrasenaPasswordBox.Clear();
+                NombreUsuarioTextBox.Focus();
+            }
+        }
+
+        private void CancelarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
