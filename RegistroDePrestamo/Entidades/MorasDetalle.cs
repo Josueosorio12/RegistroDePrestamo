aa32cba042d58671c1376274c9cb7c293cdec9d4
renydiaz02 <reny_diaz@ucne.edu.do>
2021-11-29 17:14:13 -0400

    Creando la entidad morasDetalle

---
+++ RegistroDePrestamo/Entidades/MorasDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDePrestamo.Entidades
+{
+    public class MorasDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int MoraId { get; set; }
+        public int PrestamoId { get; set; }
+        public decimal Total { get; set; }
+        public MorasDetalle(int moraId, int prestamoId, decimal total)
+        {
+            Id = 0;
+            MoraId = moraId;
+            PrestamoId = prestamoId;
+            Total = total;
+        }
+    }
+}
