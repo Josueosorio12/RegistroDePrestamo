df0471b2c6420e181f6f33b4f1440a6662c72ac6
renydiaz02 <reny_diaz@ucne.edu.do>
2021-11-29 17:12:58 -0400

    creando la entidad Mora

---
+++ RegistroDePrestamo/Entidades/Mora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDePrestamo.Entidades
+{
+    public class Mora
+    {
+        [Key]
+        public int MoraId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Total { get; set; }
+    }
+}
